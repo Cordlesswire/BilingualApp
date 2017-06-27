211d57673110bb8fa3c14f241af12ead7315c32c
Cordlesswire <chungwoo007@gmail.com>
2017-06-27 14:48:12 +0200

    Initial

--- app/src/main/java/com/example/android/miwok/ColorsFragment.java
+++ app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -60,14 +60,14 @@ public class ColorsFragment extends Fragment {
 
         final ArrayList<Word> words = new ArrayList<>();
 
-        words.add(new Word("red","weṭeṭṭi",R.drawable.color_red,R.raw.color_red));
-        words.add(new Word("green","chokokki",R.drawable.color_green,R.raw.color_green));
-        words.add(new Word("brown","ṭakaakki",R.drawable.color_brown,R.raw.color_brown));
-        words.add(new Word("gray","ṭopoppi",R.drawable.color_gray,R.raw.color_gray));
-        words.add(new Word("black","kululli",R.drawable.color_black,R.raw.color_black));
-        words.add(new Word("white","kelelli",R.drawable.color_white,R.raw.color_white));
-        words.add(new Word("dusty yellow","ṭopiisә",R.drawable.color_dusty_yellow,R.raw.color_dusty_yellow));
-        words.add(new Word("mustard yellow","chiwiiṭә",R.drawable.color_mustard_yellow,R.raw.color_mustard_yellow));
+        words.add(new Word("red","rooi",R.drawable.color_red,R.raw.color_red));
+        words.add(new Word("green","groen",R.drawable.color_green,R.raw.color_green));
+        words.add(new Word("brown","bruin",R.drawable.color_brown,R.raw.color_brown));
+        words.add(new Word("gray","grys",R.drawable.color_gray,R.raw.color_grey));
+        words.add(new Word("black","swart",R.drawable.color_black,R.raw.color_black));
+        words.add(new Word("white","wit",R.drawable.color_white,R.raw.color_white));
+        words.add(new Word("dusty yellow","stowwerige geel",R.drawable.color_dusty_yellow,R.raw.color_dusty_yellow));
+        words.add(new Word("mustard yellow","geel",R.drawable.color_mustard_yellow,R.raw.color_yellow));
 
 
         WordAdapter itemsAdapter = new WordAdapter(getActivity(),words,R.color.category_colors);
